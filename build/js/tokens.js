dad99a8b2b47e5ee4f36f1c6af5eb37c04ad774b
Rodrigo Ropelato <rodrigoropelato@hotmail.com>
2023-08-10 21:29:58 -0300

    first commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 11 Aug 2023 00:03:52 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#3a3939",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
